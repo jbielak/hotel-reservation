f35d7ff9ec159e17ed972876259ec67a2928715e
jbielak <jubi@grapeup.com>
2019-12-20 14:09:31 +0100

    update

--- src/main/java/com/grapeup/hotelreservation/repository/RoomRepository.java
+++ src/main/java/com/grapeup/hotelreservation/repository/RoomRepository.java
@@ -2,15 +2,16 @@ package com.grapeup.hotelreservation.repository;
 
 import com.grapeup.hotelreservation.model.Room;
 import com.grapeup.hotelreservation.model.RoomType;
-import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RoomRepository extends CrudRepository<Room, Long> {
-
+    //JPA find by
     @Query("SELECT r FROM Room r WHERE r.roomType = :roomType")
     List<Room> findRoomsWithCapacity(@Param("roomType") RoomType roomType);
 }
